4621ba8af506cca00d371a3296d0f57efc90e3c8
MattRob77 <Matt_Robinson1@yahoo.com>
2020-03-22 14:16:23 -0400

    finished building out charges controller to handle payments from the user. Added card error.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,21 @@
 class ChargesController < ApplicationController
+  def create
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+
+    order = Order.find(params[:orderId])
+    amount = order.sneakers.sum(:cost) * 100
+
+    charge = Stripe::Charge.create(
+      # :customer => customer.id,
+      :amount => amount,
+      :description => 'Rvive Store',
+      :currency => 'usd',
+      :source => params[:token]
+    )
+
+  rescue Stripe::CardError => e
+    flash[:errors] = e.message
+    redirect_to charges_path
+  end 
+
 end
